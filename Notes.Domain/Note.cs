91594e0d6bf6c276b3ae1ff2fec82df8e225a68c
Nikolay Gorbachev <n1kko.gorbachevv@gmail.com>
2023-12-17 17:55:01 +0300

    Add EntityFrameworkCore

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Notes.Domain
 {
-    internal class Note
+    public class Note
     {
         public Guid UserId { get; set; }
         public Guid Id { get; set; }
